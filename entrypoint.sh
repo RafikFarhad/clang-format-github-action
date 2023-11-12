7b62b41024121d4061fd101da6069b23b1fd7262
Rafik Farhad <rafikfarhad@gmail.com>
2023-11-12 15:05:00 +0600

    #7 rework + CI Added

--- entrypoint.sh
+++ entrypoint.sh
@@ -68,8 +68,12 @@ function main() {
     log "Action started"
     resolve_inputs
     log "Sources to check: $INPUT_SOURCES"
-    log "Excluding: $INPUT_EXCLUDES\n"
-    find_cmd $INPUT_SOURCES $INPUT_EXCLUDES FIND_CMD
+    if [ -n "$INPUT_EXCLUDES" ]; then
+        log "Excluding: $INPUT_EXCLUDES"
+        find_cmd "$INPUT_SOURCES" "$INPUT_EXCLUDES" FIND_CMD
+    else
+        find_cmd "$INPUT_SOURCES" "" FIND_CMD
+    fi
 
     for file in $(eval $FIND_CMD); do
         log "Checking file: $file"
